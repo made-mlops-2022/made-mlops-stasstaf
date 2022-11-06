e9d0933b39c23033d9d63a6f97b75a20958e53ba
stasstaf <stasstaf2002@gmail.com>
2022-11-07 00:27:18 +0300

    clear project

--- core/entities/feature_params.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-from typing import List, Optional
-
-
-@dataclass()
-class FeatureParams:
-    categorical_features: List[str]
-    numerical_features: List[str]
-    target_col: Optional[str]
--- core/entities/split_params.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass, field
-
-
-@dataclass()
-class SplittingParams:
-    val_size: float = field(default=0.25)
-    random_state: int = field(default=42)
-    stratify: bool = field(default=True)
